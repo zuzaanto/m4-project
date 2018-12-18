5651487c2aa5add3a9083f22e662774d773ea44b
Sergigb <sergi.garciab@e-campus.uab.cat>
2018-12-18 14:25:25 +0100

    affine + metric

--- lab1.m
+++ lab1.m
@@ -144,17 +144,19 @@ r_m1 = cross(p2, p3);
 r_l2 = l1;             %r_l2 is perpendicular to r_m2
 r_m2 = l3;
 
-r_l1 = inv(transpose(H)) * r_l1;
-r_m1 = inv(transpose(H)) * r_m1;
-r_l2 = inv(transpose(H)) * r_l2;
-r_m2 = inv(transpose(H)) * r_m2;
+% r_l1 = inv(transpose(H)) * r_l1; %??????????? why is this here?
+% r_m1 = inv(transpose(H)) * r_m1;
+% r_l2 = inv(transpose(H)) * r_l2;
+% r_m2 = inv(transpose(H)) * r_m2;
 
+figure;imshow(I);
 hold on;
 t=1:0.1:1000;
 plot(t, -(r_l1(1)*t + r_l1(3)) / r_l1(2), 'r');
 plot(t, -(r_m1(1)*t + r_m1(3)) / r_m1(2), 'b');
 plot(t, -(r_l2(1)*t + r_l2(3)) / r_l2(2), 'w');
 plot(t, -(r_m2(1)*t + r_m2(3)) / r_m2(2), 'w');
+hold off;
 
 A_ = [r_l1(1)*r_m1(1) , r_l1(1)*r_m1(2)+r_l1(2)*r_m1(1) ;
       r_l2(1)*r_m2(1) , r_l2(1)*r_m2(2)+r_l2(2)*r_m2(1) ];
@@ -168,10 +170,10 @@ S_mat = [s_vec(1),s_vec(2);s_vec(2),1];
 A = chol(S_mat);
 disp(A)
 H = zeros(3,3);
-H(1:2,1:2) = inv(A);
+H(1:2,1:2) = inv(A.'); % transpose?
 H(3,3) = 1;
 
-I3 = apply_H(I2, H);
+I3 = apply_H(I, H);
 figure; imshow(uint8(I3*255));
 
 l1_ang = atan(((-(r_l2(1)*100 + r_l2(3)) / r_l2(2)) + ((r_l2(3)) / r_l2(2)))/(100));
@@ -191,11 +193,13 @@ plot(t, -(r_l1(1)*t + r_l1(3)) / r_l1(2), 'r');
 plot(t, -(r_m1(1)*t + r_m1(3)) / r_m1(2), 'b');
 plot(t, -(r_l2(1)*t + r_l2(3)) / r_l2(2), 'w');
 plot(t, -(r_m2(1)*t + r_m2(3)) / r_m2(2), 'w');
+hold off
 
 l1_ang = atan(((-(r_l2(1)*100 + r_l2(3)) / r_l2(2)) + ((r_l2(3)) / r_l2(2)))/(100));
 l2_ang = atan(((-(r_m2(1)*100 + r_m2(3)) / r_m2(2)) +((r_m2(3)) / r_m2(2)))/(100));
 after_angle = (l1_ang - l2_ang)* 180/pi;
 disp(abs(after_angle))
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% 4. Affine and Metric Rectification of the left facade of image 0001
 
@@ -203,6 +207,8 @@ disp(abs(after_angle))
 %       the stratified method (affine + metric). 
 %       Crop the initial image so that only the left facade is visible.
 %       Show the (properly) transformed lines that use in every step.
+clear all
+close all
 
 I=imread('Data/0001_s.png');
 I = I(:,1:477,:); %crop
@@ -221,6 +227,7 @@ i = 614;
 p7 = [A(i,1) A(i,2) 1]';
 p8 = [A(i,3) A(i,4) 1]';
 
+%affine rectification
 
 l1 = cross(p1, p2);
 l2 = cross(p3, p4);
@@ -234,36 +241,102 @@ plot(t, -(l1(1)*t + l1(3)) / l1(2), 'r');
 plot(t, -(l2(1)*t + l2(3)) / l2(2), 'b');
 plot(t, -(l3(1)*t + l3(3)) / l3(2), 'g');
 plot(t, -(l4(1)*t + l4(3)) / l4(2), 'y');
+text(p1(1), p1(2), 'p1', 'Color', 'red');
+text(p2(1), p2(2), 'p2', 'Color', 'red');
+text(p3(1), p3(2), 'p3', 'Color', 'red');
+text(p4(1), p4(2), 'p4', 'Color', 'red');
+text(p5(1), p5(2), 'p5', 'Color', 'red');
+text(p6(1), p6(2), 'p6', 'Color', 'red');
+text(p7(1), p7(2), 'p7', 'Color', 'red');
+text(p8(1), p8(2), 'p8', 'Color', 'red');
+hold off;
 
-%homography
 v1 = cross(l1, l2);
 v2 = cross(l3, l4);
 l = cross(v1, v2);
 l = l / l(3);
 
-H = eye(3,3);
-H(3,:) = l;
+H_affine = eye(3,3);
+H_affine(3,:) = l;
 
-% ... metric rectification ...
+lr1 = inv(transpose(H_affine)) * l1;
+lr2 = inv(transpose(H_affine)) * l2;
+lr3 = inv(transpose(H_affine)) * l3;
+lr4 = inv(transpose(H_affine)) * l4;
 
-
-I2 = apply_H(I, H);
+I2 = apply_H(I, H_affine);
 figure; imshow(uint8(I2*255));
-
-% ToDo: compute the transformed lines lr1, lr2, lr3, lr4
-lr1 = transpose(H) \ l1;
-lr2 = transpose(H) \ l2;
-lr3 = transpose(H) \ l3;
-lr4 = transpose(H) \ l4;
-
-% show the transformed lines in the transformed image
-figure;imshow(uint8(I2*255));
 hold on;
 t=1:0.1:1000;
-plot(t, -(lr1(1)*t + lr1(3)) / lr1(2), 'y');
-plot(t, -(lr2(1)*t + lr2(3)) / lr2(2), 'y');
-plot(t, -(lr3(1)*t + lr3(3)) / lr3(2), 'y');
+plot(t, -(lr1(1)*t + lr1(3)) / lr1(2), 'r');
+plot(t, -(lr2(1)*t + lr2(3)) / lr2(2), 'b');
+plot(t, -(lr3(1)*t + lr3(3)) / lr3(2), 'g');
 plot(t, -(lr4(1)*t + lr4(3)) / lr4(2), 'y');
+hold off;
+
+%metric rectification
+
+r_l1 = cross(p1, p4);  %r_l1 is perpendicular to r_m1
+r_m1 = cross(cross(l1, l3), p3);
+r_l2 = l1;             %r_l2 is perpendicular to r_m2
+r_m2 = l4;
+
+figure;imshow(I);
+hold on;
+t=1:0.1:1000;
+plot(t, -(r_l1(1)*t + r_l1(3)) / r_l1(2), 'r');
+plot(t, -(r_m1(1)*t + r_m1(3)) / r_m1(2), 'b');
+plot(t, -(r_l2(1)*t + r_l2(3)) / r_l2(2), 'w');
+plot(t, -(r_m2(1)*t + r_m2(3)) / r_m2(2), 'w');
+hold off;
+
+A_ = [r_l1(1)*r_m1(1) , r_l1(1)*r_m1(2)+r_l1(2)*r_m1(1) ;
+      r_l2(1)*r_m2(1) , r_l2(1)*r_m2(2)+r_l2(2)*r_m2(1) ];
+b_ = [-r_l1(2)*r_m1(2) , -r_l2(2)*r_m2(2)]';
+
+s_vec = linsolve(A_,b_);
+S_mat = [s_vec(1),s_vec(2);s_vec(2),1];
+
+A = chol(S_mat);
+disp(A)
+H_met = zeros(3,3);
+H_met(1:2,1:2) = inv(A.'); % transpose????
+H_met(3,3) = 1;
+
+l1_ang = atan(((-(r_l2(1)*100 + r_l2(3)) / r_l2(2)) + ((r_l2(3)) / r_l2(2)))/(100));
+l2_ang = atan(((-(r_m2(1)*100 + r_m2(3)) / r_m2(2)) +((r_m2(3)) / r_m2(2)))/(100));
+before_angle = (l1_ang - l2_ang)* 180/pi;
+disp(abs(before_angle))
+
+H = H_met * H_affine;
+
+%can't plot these lines right
+r_l1 = inv(transpose(H)) * r_l1;
+r_m1 = inv(transpose(H)) * r_m1;
+r_l2 = inv(transpose(H)) * r_l2;
+r_m2 = inv(transpose(H)) * r_m2;
+
+% show the transformed lines in the transformed image
+% I3 = apply_H(I, H_affine);
+% I3 = apply_H(I3, H_met);
+% figure; imshow(uint8(I3*255));
+
+I3 = apply_H(I, H);
+figure; imshow(uint8(I3*255));
+
+hold on;
+t=1:0.1:1000;
+plot(t, -(r_l1(1)*t + r_l1(3)) / r_l1(2), 'r');
+plot(t, -(r_m1(1)*t + r_m1(3)) / r_m1(2), 'b');
+plot(t, -(r_l2(1)*t + r_l2(3)) / r_l2(2), 'w');
+plot(t, -(r_m2(1)*t + r_m2(3)) / r_m2(2), 'w');
+hold off
+
+l1_ang = atan(((-(r_l2(1)*100 + r_l2(3)) / r_l2(2)) + ((r_l2(3)) / r_l2(2)))/(100));
+l2_ang = atan(((-(r_m2(1)*100 + r_m2(3)) / r_m2(2)) +((r_m2(3)) / r_m2(2)))/(100));
+after_angle = (l1_ang - l2_ang)* 180/pi;
+disp(abs(after_angle))
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% 5. OPTIONAL: Metric Rectification in a single step
