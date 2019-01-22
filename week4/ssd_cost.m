cdcc88e0fe3649b697d84618c0829d430447fc9a
Basem Elbarashy <es-basem.abdelghafar1116@alexu.edu.eg>
2019-01-22 17:13:15 +0100

    fix for 2015m

--- week4/ssd_cost.m
+++ week4/ssd_cost.m
@@ -2,8 +2,7 @@ function [cost] = ssd_cost(x1,x2)
 %SSD_COST Summary of this function goes here
 %   Detailed explanation goes here
 w = 1/(size(x1,1)*size(x1,2));
-
-cost = sum(w*(x1-x2).^2,'all');
-
+%cost = sum(w*(x1-x2).^2,'all');
+cost = sum(sum(w*(x1-x2).^2));
 end
 
